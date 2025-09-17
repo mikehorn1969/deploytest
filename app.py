5a1aa0b0a8a4696a975956d68caeca70b16726e5
Mike Horn <mike@mikehorn.net>
2025-09-17 10:33:16 +0100

    Refactor app.run configuration to use environment variable for port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def home():
     return "<div><H1>Work in progress</H1></div><div><H3>CS Document Generator coming soon!</H3></div>"
 
 if __name__ == '__main__':
-    app.run(port=80,host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
